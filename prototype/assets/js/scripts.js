eb553d6680081668581f8eec338d0a09922ea3d6
Hilton Williams <hiltonwilliams@sanmar.com>
2019-08-10 02:44:32 -0700

    Add cross browser compatible smooth scroll feature.

--- prototype/assets/js/scripts.js
+++ prototype/assets/js/scripts.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
 
   const backToTopButton = document.querySelector('.back-to-top');
+  const scrollLinks = document.querySelectorAll('.scroll-link');
 
   window.addEventListener('scroll', scrollFunction);
 
@@ -13,15 +14,52 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   }
 
-  backToTopButton.addEventListener('click', backToTop);
+  backToTopButton.addEventListener('click', smoothScrollBackToTop);
 
-  function backToTop(event) { // Scroll Back to the top of the document
-   window.scrollTo(0, 0);
-   event.preventDefault();
+  function smoothScrollBackToTop() {
+    const targetPosition = 0;
+    const startPosition = window.pageYOffset;
+    const distance = targetPosition - startPosition;
+    const duration = 750;
+    let start = null;
+
+    window.requestAnimationFrame(step);
+
+    function step(timestamp) {
+      if (!start) start = timestamp;
+      const progress = timestamp - start;
+      window.scrollTo(0, distance*(progress/duration) + startPosition);
+      if (progress < duration) {
+        window.requestAnimationFrame(step);
+      }
+    }
+
+  }
+
+  for(let i=0; i<scrollLinks.length; i++) {
+    scrollLinks[i].addEventListener('click', smoothScroll);
   }
 
   // smooth scrolling
   function smoothScroll(event) {
+    event.preventDefault();
+    const targetId = event.currentTarget.getAttribute('href');
+    const targetPosition = document.querySelector(targetId).offsetTop;
+    const startPosition = window.pageYOffset;
+    const distance = targetPosition - startPosition;
+    const duration = 1000;
+    let start = null;
+
+    window.requestAnimationFrame(step);
+
+    function step(timestamp) {
+      if (!start) start = timestamp;
+      const progress = timestamp - start;
+      window.scrollTo(0, distance*(progress/duration) + startPosition);
+      if (progress < duration) {
+        window.requestAnimationFrame(step);
+      }
+    }
 
   }
 
