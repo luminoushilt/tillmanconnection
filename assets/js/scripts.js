0348bcc13e7b72ffd06485da259f6396a6a5edb7
Hilton Williams <hwilliams@luminoushilt.com>
2020-07-09 23:21:54 -0700

    Attempt to add meta image to website prototype.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,22 +1,20 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const backToTopButton = document.querySelector(".back-to-top");
-  const scrollLinks = document.querySelectorAll(".scroll-link");
-  const dropDown = document.querySelector(".drop-down-arrow");
-  const dropDownMenu = document.querySelector(".navbar-sub-menu");
+document.addEventListener('DOMContentLoaded', function() {
 
-  window.addEventListener("scroll", scrollFunction);
+  const backToTopButton = document.querySelector('.back-to-top');
+  const scrollLinks = document.querySelectorAll('.scroll-link');
+
+  window.addEventListener('scroll', scrollFunction);
 
   function scrollFunction() {
-    if (window.pageYOffset > 300) {
-      // Show backToTopButton
-      backToTopButton.style.display = "block";
-    } else {
-      // Hide backToTopButton
-      backToTopButton.style.display = "none";
+    if(window.pageYOffset > 300) { // Show backToTopButton
+     backToTopButton.style.display = 'block';
+    }
+    else { // Hide backToTopButton
+     backToTopButton.style.display = 'none';
     }
   }
 
-  backToTopButton.addEventListener("click", smoothScrollBackToTop);
+  backToTopButton.addEventListener('click', smoothScrollBackToTop);
 
   function smoothScrollBackToTop() {
     const targetPosition = 0;
@@ -30,21 +28,22 @@ document.addEventListener("DOMContentLoaded", function() {
     function step(timestamp) {
       if (!start) start = timestamp;
       const progress = timestamp - start;
-      window.scrollTo(0, distance * (progress / duration) + startPosition);
+      window.scrollTo(0, distance*(progress/duration) + startPosition);
       if (progress < duration) {
         window.requestAnimationFrame(step);
       }
     }
+
   }
 
-  for (let i = 0; i < scrollLinks.length; i++) {
-    scrollLinks[i].addEventListener("click", smoothScroll);
+  for(let i=0; i<scrollLinks.length; i++) {
+    scrollLinks[i].addEventListener('click', smoothScroll);
   }
 
   // smooth scrolling
   function smoothScroll(event) {
     event.preventDefault();
-    const targetId = event.currentTarget.getAttribute("href");
+    const targetId = event.currentTarget.getAttribute('href');
     const targetPosition = document.querySelector(targetId).offsetTop;
     const startPosition = window.pageYOffset;
     const distance = targetPosition - startPosition;
@@ -56,17 +55,12 @@ document.addEventListener("DOMContentLoaded", function() {
     function step(timestamp) {
       if (!start) start = timestamp;
       const progress = timestamp - start;
-      window.scrollTo(0, distance * (progress / duration) + startPosition);
+      window.scrollTo(0, distance*(progress/duration) + startPosition);
       if (progress < duration) {
         window.requestAnimationFrame(step);
       }
     }
+
   }
 
-  dropDown.addEventListener("click", openDropDownMenu);
-
-  // Drop Down Menu
-  function openDropDownMenu(event) {
-    dropDownMenu.classList.toggle("menu-open");
-  }
-});
+ });
